f242b5cfb60d086ba59518995358717e5f572610
EstherBaum <134812282+EstherBaum@users.noreply.github.com>
2023-09-20 21:33:13 -0500

    Add files via upload

---
+++ Project 2.sql
@@ -0,0 +1,55 @@
+DROP TABLE category;
+
+CREATE TABLE category (
+	category_id VARCHAR PRIMARY KEY,
+	category VARCHAR
+);
+	
+DROP TABLE subcategory;
+
+CREATE TABLE subcategory (
+	subcategory_id VARCHAR PRIMARY KEY,
+	subcategory VARCHAR
+);
+
+DROP TABLE campaign;
+
+CREATE TABLE campaign (
+	cf_id INT PRIMARY KEY,
+	contact_id VARCHAR,
+	company_name VARCHAR,
+	description VARCHAR,
+	goal VARCHAR,
+	pledged VARCHAR,
+	outcome VARCHAR,
+	backers_count VARCHAR,
+	country VARCHAR,
+	currency VARCHAR,
+	launched_date VARCHAR,
+	end_date VARCHAR,
+	category_id VARCHAR,
+	subcategory_id VARCHAR
+);
+
+DROP contacts;
+
+CREATE TABLE contacts (
+	contact_id INT PRIMARY KEY,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	email VARCHAR
+);
+
+SELECT * 
+FROM category
+
+SELECT *
+FROM subcategory
+
+SELECT *
+FROM campaign
+
+SELECT * 
+FROM contacts
+
+
