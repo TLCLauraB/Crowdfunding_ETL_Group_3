05b531fc3145317d4395d8877599adb614f7aa29
JacobMoen <134341713+JacobMoen@users.noreply.github.com>
2023-09-22 15:21:44 -0500

    Add files via upload

---
+++ DBD Map.sql
@@ -0,0 +1,58 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(200)   NOT NULL,
+    "description" VARCHAR(200)   NOT NULL,
+    "goal" INT   NOT NULL,
+    "pledged" INT   NOT NULL,
+    "outcome" VARCHAR(200)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(200)   NOT NULL,
+    "currency" VARCHAR(200)   NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(200)   NOT NULL,
+    "subcategory_id" VARCHAR(200)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR(200)   NOT NULL,
+    "category" VARCHAR(200)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "name" VARCHAR(200)   NOT NULL,
+    "email" VARCHAR(200)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "subcategories" (
+    "subcategory_id" VARCHAR(200)   NOT NULL,
+    "subcategory" VARCHAR(200)   NOT NULL,
+    CONSTRAINT "pk_subcategories" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategories" ("subcategory_id");
+
+ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "campaign" ("contact_id");
+
