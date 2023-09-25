05dafbe7ab2bab69b7009156c7083673f27b7ffe
Jake Moen <jakemoen88@gmail.com>
2023-09-24 20:26:44 -0500

    sql databae, ERD, schema and tables

--- Postgres file crowdfunfing_db_schema.sql
+++ Postgres file crowdfunfing_db_schema.sql
@@ -1,26 +1,6 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-
-CREATE TABLE "campaign" (
-    "cf_id" INT   NOT NULL,
-    "contact_id" INT   NOT NULL,
-    "company_name" VARCHAR(200)   NOT NULL,
-    "description" VARCHAR(200)   NOT NULL,
-    "goal" INT   NOT NULL,
-    "pledged" INT   NOT NULL,
-    "outcome" VARCHAR(200)   NOT NULL,
-    "backers_count" INT   NOT NULL,
-    "country" VARCHAR(200)   NOT NULL,
-    "currency" VARCHAR(200)   NOT NULL,
-    "launched_date" DATE   NOT NULL,
-    "end_date" DATE   NOT NULL,
-    "category_id" VARCHAR(200)   NOT NULL,
-    "subcategory_id" VARCHAR(200)   NOT NULL,
-    CONSTRAINT "pk_campaign" PRIMARY KEY (
-        "cf_id"
-     )
-);
 
 CREATE TABLE "category" (
     "category_id" VARCHAR(200)   NOT NULL,
@@ -32,7 +12,8 @@ CREATE TABLE "category" (
 
 CREATE TABLE "contacts" (
     "contact_id" INT   NOT NULL,
-    "name" VARCHAR(200)   NOT NULL,
+    "first_name" VARCHAR(200)   NOT NULL,
+	"last_name" VARCHAR(200)   NOT NULL,
     "email" VARCHAR(200)   NOT NULL,
     CONSTRAINT "pk_contacts" PRIMARY KEY (
         "contact_id"
@@ -47,12 +28,32 @@ CREATE TABLE "subcategories" (
      )
 );
 
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(200)   NOT NULL,
+    "description" VARCHAR(200)   NOT NULL,
+    "goal" DECIMAL   NOT NULL,
+    "pledged" DECIMAL   NOT NULL,
+    "outcome" VARCHAR(200)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(200)   NOT NULL,
+    "currency" VARCHAR(200)   NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(200)   NOT NULL,
+    "subcategory_id" VARCHAR(200)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
 REFERENCES "category" ("category_id");
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategories" ("subcategory_id");
-
-ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "campaign" ("contact_id");
 
